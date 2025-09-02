d51c027a5aab9c9ce00b71fe8e6db415bf2e173e
swetharameshrms2007-byte <swetharameshrms2007@gmail.com>
2025-09-02 14:26:33 +0530

    Delete regscript.js

--- regscript.js
+++
@@ -1,7 +0,0 @@
-// Simple form validation & submission alert
-document.getElementById("registrationForm").addEventListener("submit", function (event) {
-  event.preventDefault(); // stop form from refreshing
-
-  alert("🎉 Registration Successful! Thank you for registering.");
-  this.reset(); // clears the form after submit
-});

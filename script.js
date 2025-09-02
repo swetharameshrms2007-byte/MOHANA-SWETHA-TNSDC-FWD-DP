6fe8759f5a192a55b216adead1b11a6c3fbc9315
swetharameshrms2007-byte <swetharameshrms2007@gmail.com>
2025-09-02 14:23:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Handle resume download
-document.getElementById("downloadBtn").addEventListener("click", function () {
-  // Replace with your actual resume file path
-  const link = document.createElement("a");
-  link.href = "resume.pdf"; // <-- put your resume file in the same folder
-  link.download = "Mohanaswetha_Resume.pdf";
-  link.click();
-});
